acd4d8d35de189714baff98eca6e9fc16b01886b
V S <vatosarmat@yandex.ru>
2023-03-22 15:48:48 +1000

    Enter

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
 
+    'prefer-const': 'warn',
+
     '@typescript-eslint/ban-types': [
       'warn',
       {
--- src/state/calculator/input/selector.ts
+++ src/state/calculator/input/selector.ts
@@ -5,12 +5,34 @@ export const numberValue = ({ integer, fraction, minus }: State): number => {
   return minus ? -abs : abs
 }
 
+const toChunkedString = (num: string, chunkLen = 3, sep = ' '): string => {
+  if (num.length < chunkLen) {
+    return num
+  }
+
+  const prefix = num.slice(0, num.length % chunkLen)
+  let ret = ''
+  for (let i = prefix.length; i < num.length; i += chunkLen) {
+    ret += sep + num.slice(i, i + chunkLen)
+  }
+
+  if (prefix.length === 0) {
+    return ret.slice(sep.length)
+  }
+
+  return prefix + ret
+}
+
 export const stringValue = ({ integer, fraction, minus }: State): string => {
   let str
+
+  const intStr = toChunkedString(integer)
+
   if (fraction !== null) {
-    str = `${integer}.${fraction}`
+    const fracStr = toChunkedString(fraction)
+    str = `${intStr}.${fracStr}`
   } else {
-    str = integer.toString()
+    str = intStr
   }
   return minus ? '-' + str : str
 }
